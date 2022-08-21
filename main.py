853014c725c4f050e2f39fb00edabcbbd63887fb
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-08-21 14:38:32 +0100

    done

--- main.py
+++ main.py
@@ -1,9 +1,62 @@
+from calendar import c
+
+
 names = {
-        1:"Abu",
-        2:"ali",
-        3:"hauwa",
-        4:"kopat"
+    "Abu": 81,
+    "ali": 78,
+    "hauwa": 99,
+    "kopat": 65
+}
+
+# list in dictionry
+travel_log = {
+    "France": {
+        "cities_visited":["paris","Lile","Dijon"],
+        "To Consider":["Kano","Kaduna"]
+        },
+    "Nigeria": {
+        "cities_visited":["Kaduna","Kano","Zamfara","Katsina","Kebbi","Sokoto","Abuja"],"total state visited":7
     }
-for i in names.keys():
-    print(names[i])
+}
+
+cities = ["Kaduna", "Kano", "Zamfara", "Katsina", "Kebbi", "Sokoto", "Abuja"]
+
+
+#dictionry in list
+
+travel_log1 = [
+    {
+       "Country": "Nigeria",
+       "cities_visited": cities,
+       "totalCity visited": len(cities)
+       
+    },
+    {
+        "Country": "Nigeria",
+        "cities_visited": cities,
+        "totalCity visited": len(cities)
+    }
     
+]
+   
+    
+studentGrades = {}
+
+for i in names:
+    score = names[i]
+    if names[i] >= 91:
+        studentGrades[i] = "Outstanding"
+    elif names[i] >= 81:
+        studentGrades[i] = "Excelent"
+    elif names[i] >= 71:
+        studentGrades[i] = "Good"
+    else:
+        studentGrades[i] = "making progress"
+        
+for grade in studentGrades:
+    print(studentGrades[grade])
+    print("")
+for city in travel_log:
+    for i  in travel_log[city]:
+        print(travel_log[city][i])
+print(travel_log1)
